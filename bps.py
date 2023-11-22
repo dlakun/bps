305a9d4ed4157292f3ad5b6202092e5b02097df9
dlakun <151622171+dlakun@users.noreply.github.com>
2023-11-22 09:01:08 +0000

    1

--- bps.py
+++ bps.py
@@ -1,46 +1,67 @@
-# shopee.py
+#Import Packages
+from pandas import ExcelWriter
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
-import re
-import csv
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
+import pandas as pd
 import time
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('log-level=2')
-driver = webdriver.Chrome()
-
-
 #Setup WebDriver
-#option = webdriver.ChromeOptions()
-#option.add_argument("--headless")
-#service = Service()
-#driver = webdriver.Chrome(service=service, options=option)
-katakunci = input('Masukkan kata kunci : ')
+option = webdriver.ChromeOptions()
+option.add_argument("--headless")
+service = Service()
 
-def search(katakunci):
-    links = []
-    print('mencari semua product dengan kata kunci ' + katakunci)
-    url = 'https://shopee.co.id/search?keyword=' + katakunci
-    try:
-        driver.get(url)
-        time.sleep(5)
-        driver.execute_script('window.scrollTo(0, 1500);')
-        time.sleep(5)
-        driver.execute_script('window.scrollTo(0, 2500);')
-        time.sleep(5)
-        soup_a = BeautifulSoup(driver.page_source, 'html.parser')
-        products = soup_a.find('div', class_='row shopee-search-item-result__items')
-        for link in products.find_all('a'):
-            links.append(link.get('href'))
-            print(link.get('href'))
-    except TimeoutException:
-        print('failed to get links with query ' + line)
-    return links
+driver = webdriver.Chrome(service=service, options=option)
 
-product_urls = search(katakunci)
+#Scraping Data
+shopee_link = "https://shopee.co.id/search?keyword=laptop"
+driver.set_window_size(1300, 800)
+driver.get(shopee_link)
+
+distance = 500
+for i in range(1, 7):
+    last = distance * i
+    command = "window.scrollTo(0," + str(last) + ")"
+    driver.execute_script(command)
+    print("loading to-" + str(i))
+    time.sleep(1)
+
+time.sleep(5)
+driver.save_screenshot("home.png")
+content = driver.page_source
+driver.quit()
+
+data = BeautifulSoup(content, "html.parser")
+
+#Extracting Data
+i = 1
+base_url = "https://shopee.co.id"
+
+list_name, list_price, list_link, list_sold, list_location = [], [], [], [], []
+
+
+for area in data.find_all("div", class_="col-xs-2-4 shopee-search-item-result__item"):
+    print("processing data to-" + str(i))
+    name = area.find("div", class_="ie3A+n bM+7UW Cve6sh").get_text()
+    price = area.find("span", class_="ZEgDH9").get_text()
+    link = base_url + area.find("a")["href"]
+    sold = area.find("div", class_="r6HknA uEPGHT")
+    if sold is not None:
+        sold = sold.get_text()
+    location = area.find("div", class_="zGGwiV").get_text()
+
+    list_name.append(name)
+    list_price.append(price)
+    list_link.append(link)
+    list_sold.append(sold)
+    list_location.append(location)
+    i += 1
+    print("------")
+
+#Storing Data
+df = pd.DataFrame({"Name": list_name, "Price": list_price, "Link": list_link, "Sold": list_sold,
+                   "Location": list_location})
+writer: ExcelWriter = pd.ExcelWriter("/workspaces/bps/Laptop.xlsx")
+
+df.to_excel(writer, "Sheet1", index=False)
+writer.save()
